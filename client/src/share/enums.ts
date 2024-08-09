8cbec47c42fab032d791b4ba7a1989e6356c45ce
niu-grandpa <2864103063@qq.com>
2024-08-09 16:13:04 +0800

    re: 重构枚举值文件

--- client/src/share/enums.ts
+++ client/src/share/enums.ts
@@ -1,10 +1,17 @@
+export enum LOCAL_ITEM_KEY {
+  FILE_DATA = 'FILE_DATA',
+  OPENED_KEYS = 'OPENED_KEYS',
+  SELECTED_KEYS = 'SELECTED_KEYS',
+  EXPANED_KEYS = 'EXPANED_KEYS',
+  NOTIFICATION = 'NOTIFICATION',
+}
+
 export enum ANODE_ACTION_KEY {
   CREATE_FOLDER = 'folder',
   CREATE_FILE = 'file',
   CUT = 'cut',
   COPY = 'copy',
   PASTE = 'paste',
-  REFRESH = 'refresh',
   RENAME = 'rename',
   DELETE = 'delete',
   VIEW_CODE = 'viewCode',
@@ -16,7 +23,6 @@ export const ANodeActionTitles = {
   [ANODE_ACTION_KEY.CUT]: '剪切',
   [ANODE_ACTION_KEY.COPY]: '复制',
   [ANODE_ACTION_KEY.PASTE]: '粘贴',
-  [ANODE_ACTION_KEY.REFRESH]: '刷新',
   [ANODE_ACTION_KEY.RENAME]: '重命名...',
   [ANODE_ACTION_KEY.DELETE]: '删除',
 };
