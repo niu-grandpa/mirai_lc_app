6f2ea0be1b49d2ff2325745423384e51313c5dfa
niu-grandpa <2864103063@qq.com>
2024-08-25 13:21:50 +0800

    test：更新测试逻辑

--- server/__tests__/create_download_file.ts
+++ server/__tests__/create_download_file.ts
@@ -1,10 +1,24 @@
 import { describe } from '@jest/globals';
 import test from 'node:test';
-import controller from '../src/controllers/download';
-import { type FolderANode } from '../src/types/abstract_node';
+import { DownloadController } from '../src/controllers/download';
+import { type FileANode, type FolderANode } from '../src/types/abstract_node';
 
-describe('测试 DownloadController.create', () => {
-  test('应创建一个包含 .vue 的项目zip包', async () => {
+describe('DownloadController 测试用例', () => {
+  class DownloadControllerTest extends DownloadController {
+    constructor() {
+      super();
+    }
+    testCreateWholeProject(data: FolderANode) {
+      return this._createOneWhole(data);
+    }
+    testCreateOneFile(data: FileANode) {
+      return this._createOneFile(data);
+    }
+  }
+
+  const controller = new DownloadControllerTest();
+
+  test('生成项目zip包', async () => {
     const mock: FolderANode = {
       key: 'zYYfHhDg-;fld',
       name: 'views',
@@ -83,8 +97,81 @@ describe('测试 DownloadController.create', () => {
     };
 
     try {
-      const res = await controller._testCreateFn(mock);
-      console.log('result: ', res);
+      await controller.testCreateWholeProject(mock);
+    } catch {}
+  });
+
+  test('生成单个vue文件', async () => {
+    const mock = {
+      isLeaf: true,
+      isFile: true,
+      key: 'zHwf2eg2L',
+      name: 'Login',
+      anodes: [
+        {
+          isLeaf: true,
+          key: 'zAg_e6Heh',
+          name: 'div',
+          isComp: false,
+          x: 10,
+          y: 20,
+          children: [],
+          textContent: 'Example content',
+          attrs: {
+            class: {
+              aadas: true,
+            },
+            style: {
+              width: '120px',
+              height: '60px',
+              position: 'absolute',
+              transform: 'translate(10px, 20px)',
+              color: '#fff',
+              fontSize: '16px',
+              backgroundColor: '#29e',
+            },
+          },
+          props: {},
+          directives: {},
+          on: {
+            click: () => {
+              const num = 1;
+              console.log(num);
+            },
+          },
+        },
+        {
+          isLeaf: true,
+          key: 'zm_op2jmq',
+          name: 'div',
+          isComp: false,
+          x: 0,
+          y: 0,
+          children: [],
+          textContent: 'Example content2',
+          attrs: {
+            class: {
+              aadas: true,
+            },
+            style: {
+              width: '120px',
+              height: '60px',
+              position: 'absolute',
+              transform: 'translate(0, 0)',
+              color: '#fff',
+              fontSize: '16px',
+              backgroundColor: '#29e',
+            },
+          },
+          props: {},
+          directives: {},
+          on: {},
+        },
+      ],
+    };
+
+    try {
+      await controller.testCreateOneFile(mock);
     } catch {}
   });
 });
