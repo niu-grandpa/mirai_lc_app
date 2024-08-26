14ff833e09ee6d97e130037f9d7a9392930ccf7b
niu-grandpa <2864103063@qq.com>
2024-08-26 10:43:56 +0800

    re：删除多余的前缀路径

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -3,6 +3,6 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.post('/user/register', controller.register);
+router.post('/register', controller.register);
 
 export default router;
