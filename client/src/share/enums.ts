48ad381e06c3949000051e04958208a95bef1825
niu-grandpa <2864103063@qq.com>
2024-08-27 22:02:40 +0800

    re：枚举值修改

--- client/src/share/enums.ts
+++ client/src/share/enums.ts
@@ -13,6 +13,7 @@ export enum ANODE_ACTION_KEY {
   CUT = 'cut',
   COPY = 'copy',
   PASTE = 'paste',
+  DOWNLOAD = 'download',
   RENAME = 'rename',
   DELETE = 'delete',
   VIEW_CODE = 'viewCode',
@@ -24,6 +25,13 @@ export const ANodeActionTitles = {
   [ANODE_ACTION_KEY.CUT]: '剪切',
   [ANODE_ACTION_KEY.COPY]: '复制',
   [ANODE_ACTION_KEY.PASTE]: '粘贴',
+  [ANODE_ACTION_KEY.DOWNLOAD]: '下载',
   [ANODE_ACTION_KEY.RENAME]: '重命名...',
   [ANODE_ACTION_KEY.DELETE]: '删除',
 };
+
+export enum DOWNLOAD_FILE_TYPE {
+  VUE = 'VUE',
+  HTML = 'HTML',
+  JSON = 'JSON',
+}
