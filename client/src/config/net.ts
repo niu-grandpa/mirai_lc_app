f3e988f256ec9b3b57b1a3cebb0145a833dcb592
niu-grandpa <2864103063@qq.com>
2024-08-27 22:02:18 +0800

    re：网络请求自定义配置

---
+++ client/src/config/net.ts
@@ -0,0 +1,13 @@
+import { getLocalItem } from '@/share';
+import { type AxiosRequestConfig } from 'axios';
+
+export default {
+  method: 'GET',
+  timeout: 2000,
+  headers: {
+    Authorization: getLocalItem('USER_TOKEN'),
+  },
+  baseURL: `${import.meta.env.PUBLIC_PROXY}${
+    import.meta.env.PUBLIC_API_PREFIX
+  }`,
+} as AxiosRequestConfig;
