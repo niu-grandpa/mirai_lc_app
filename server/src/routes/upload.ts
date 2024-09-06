10d7775714f18343cb6ad10e0da805a502c19232
niu-grandpa <2864103063@qq.com>
2024-09-06 15:57:08 +0800

    re：设置 multer 中间件处理上传json文件的请求

--- server/src/routes/upload.ts
+++ server/src/routes/upload.ts
@@ -1,8 +1,14 @@
 import controller from '@/controllers/upload';
 import { Router } from 'express';
+import multer from 'multer';
+import path from 'path';
 
 const router = Router();
 
-router.post('/work-file', controller.readJsonFile);
+const upload = multer({
+  dest: path.join(__dirname, '../../public/uploads/'),
+});
+
+router.post('/work-file', upload.single('jsonFile'), controller.readJsonFile);
 
 export default router;
