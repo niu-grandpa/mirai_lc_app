5e3c110110567301e5239453d9b95860fa7510e7
niu-grandpa <2864103063@qq.com>
2024-09-06 15:58:15 +0800

    re：读取json文件内容返回给前端

--- server/src/controllers/upload.ts
+++ server/src/controllers/upload.ts
@@ -1,10 +1,26 @@
 import { RouteError, type IReq, type IRes } from '@/types/types';
 import HttpStatusCodes from 'constants/http_status_codes';
+import fs from 'fs-extra';
 
 export class UploadController {
   readJsonFile = async (req: IReq, res: IRes): Promise<IRes> => {
     try {
-      return res.status(HttpStatusCodes.OK).json({});
+      const file = req.file;
+      if (file?.mimetype !== 'application/json') {
+        return res
+          .status(HttpStatusCodes.BAD_REQUEST)
+          .json({ error: '请上传json文件' });
+      }
+
+      let jsonData = {};
+      fs.readFile(file.path, 'utf8', (err, data) => {
+        if (!err) {
+          jsonData = JSON.parse(data);
+          fs.removeSync(file.path);
+        }
+      });
+
+      return res.status(HttpStatusCodes.OK).json({ data: jsonData });
     } catch (e) {
       throw new RouteError(HttpStatusCodes.INTERNAL_SERVER_ERROR, e.message);
     }
