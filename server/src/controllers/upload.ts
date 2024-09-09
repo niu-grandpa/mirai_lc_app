531004863cb5ec3f3defc1096a8fb7f4922656fb
niu-grandpa <2864103063@qq.com>
2024-09-09 17:15:47 +0800

    re：更改为同步读取文件内容

--- server/src/controllers/upload.ts
+++ server/src/controllers/upload.ts
@@ -6,21 +6,17 @@ export class UploadController {
   readJsonFile = async (req: IReq, res: IRes): Promise<IRes> => {
     try {
       const file = req.file;
+
       if (file?.mimetype !== 'application/json') {
         return res
           .status(HttpStatusCodes.BAD_REQUEST)
           .json({ error: '请上传json文件' });
       }
 
-      let jsonData = {};
-      fs.readFile(file.path, 'utf8', (err, data) => {
-        if (!err) {
-          jsonData = JSON.parse(data);
-          fs.removeSync(file.path);
-        }
-      });
+      const content = fs.readFileSync(file.path, 'utf8');
+      fs.remove(file.path);
 
-      return res.status(HttpStatusCodes.OK).json({ data: jsonData });
+      return res.status(HttpStatusCodes.OK).json({ data: content });
     } catch (e) {
       throw new RouteError(HttpStatusCodes.INTERNAL_SERVER_ERROR, e.message);
     }
