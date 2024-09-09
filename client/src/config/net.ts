6536d21ed1f35455a5dea0ab21ad235a4542f492
niu-grandpa <2864103063@qq.com>
2024-09-09 17:17:30 +0800

    re：新增请求配置

--- client/src/config/net.ts
+++ client/src/config/net.ts
@@ -4,8 +4,12 @@ import { type AxiosRequestConfig } from 'axios';
 export default {
   method: 'GET',
   timeout: 10000,
+  withCredentials: true,
+  responseType: 'json',
+  withXSRFToken: true,
   headers: {
     Authorization: getLocalItem('USER_TOKEN'),
+    'Content-Type': 'application/json',
   },
   baseURL: `${import.meta.env.PUBLIC_PROXY}${
     import.meta.env.PUBLIC_API_PREFIX
