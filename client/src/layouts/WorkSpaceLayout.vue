4f76fbffe3ae734238305460c47fe2e0c5dda09c
niu-grandpa <2864103063@qq.com>
2024-09-13 15:03:07 +0800

    feat：新增允许侧边栏自由拖动自定义宽度的功能

--- client/src/layouts/WorkSpaceLayout.vue
+++ client/src/layouts/WorkSpaceLayout.vue
@@ -1,17 +1,70 @@
 <template>
   <a-layout>
     <a-layout>
-      <a-layout-content :style="styles">
+      <a-layout-content>
         <slot name="content" />
       </a-layout-content>
     </a-layout>
-    <a-layout-sider :width="370" :style="styles">
+    <a-layout-sider
+      ref="siderRef"
+      :data-x="dataX"
+      :width="store.width"
+      :style="{ padding: 0, position: 'relative', background: '#24292E' }">
+      <div class="resize-bar" ref="resizeBarRef" />
       <slot name="sider" />
     </a-layout-sider>
   </a-layout>
 </template>
 
-<script setup>
-import { computed } from 'vue';
-const styles = computed(() => ({ padding: '0', background: '#24292E' }));
+<script setup lang="ts">
+import { useSidebarStore } from '@/stores/sidebarStore';
+import interact from 'interactjs';
+import { onMounted, ref } from 'vue';
+
+const store = useSidebarStore();
+
+const dataX = ref(0);
+const siderRef = ref();
+const resizeBarRef = ref<HTMLElement>();
+
+const handleSiderResize = () => {
+  interact(siderRef.value!.$el).resizable({
+    edges: {
+      top: false,
+      bottom: false,
+      left: resizeBarRef.value!,
+      right: false,
+    },
+    invert: 'reposition',
+    cursorChecker: () => 'col-resize',
+    onmove: event => {
+      let x = dataX.value;
+      x = ~~((x || 0) - event.deltaRect.left);
+      dataX.value = x;
+      store.setWidth(store.initWidth + x);
+    },
+    onend: () => {
+      dataX.value = 0;
+      store.updateInitWidth();
+    },
+  });
+};
+
+onMounted(handleSiderResize);
 </script>
+
+<style scoped>
+.resize-bar {
+  width: 5px;
+  height: 100%;
+  position: absolute;
+  z-index: 1;
+  transition: background 0.3s ease-in-out;
+  touch-action: none;
+  user-select: none;
+
+  &:hover {
+    background-color: #1e8fffb7;
+  }
+}
+</style>
