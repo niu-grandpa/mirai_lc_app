b55a2744469ccf6c9f5f7d41c01307b763ba8bda
niu-grandpa <2864103063@qq.com>
2024-09-16 13:19:02 +0800

    feat:  新增涵盖上下左右8个方向的辅助线对齐功能

--- client/src/views/WorkSpace/components/visual/AuxLines.vue
+++ client/src/views/WorkSpace/components/visual/AuxLines.vue
@@ -35,12 +35,63 @@ const resetAlignment = () => {
   showHorizontalLine.value = false;
 };
 
+const checkIfWithinThreshold = (current: number, target: number) => {
+  return Math.abs(current - target) < alignmentThreshold;
+};
+
+/**预判阈值 */
 const alignmentThreshold = DRAG_RANGE * 2;
 
-function checkIfWithinThreshold(current: number, target: number) {
-  return Math.abs(current - target) < alignmentThreshold;
-}
-
+/**
+ * @function updateAlignment
+ *
+ * @name 水平方向计算相邻元素
+ *
+ * @description
+ * 1.从左到右判断相邻节点
+ *
+ * 1.1.计算是否靠近左侧：
+ * 用当前节点的右边界值挨个减去其他节点的左边界值，如果小于24表示即将接近某个节点的左侧，
+ * 将垂直辅助线的的x坐标值赋值为相邻节点的左边界值。
+ *
+ * 1.2.计算是否靠近右侧：
+ * 用当前节点的右边界值挨个减去其他节点的右边界值，如果小于24表示即将接近某个节点的右侧，
+ * 将垂直辅助线的的x坐标值赋值为相邻节点的右边界值。
+ *
+ * 2.从右到左判断相邻节点
+ *
+ * 2.1.计算是否靠近右侧：
+ * 用当前节点的左边界值挨个减去其他节点的右边界值，如果小于24表示即将接近某个节点的右侧，
+ * 将垂直辅助线的的x坐标值赋值为相邻节点的右边界值。
+ *
+ * 2.2.计算是否靠近左侧：
+ * 用当前节点的左边界值挨个减去其他节点的左边界值，如果小于24表示即将接近某个节点的左侧，
+ * 将垂直辅助线的的x坐标值赋值为相邻节点的左边界值。
+ *
+ * @name 垂直方向计算相邻元素
+ *
+ * @description
+ * 3.从上到下判断相邻节点
+ *
+ * 3.1.计算是否靠近顶部：
+ * 用当前节点的下边界值挨个减去其他节点的上边界值，如果小于24表示即将接近某个节点的顶部，
+ * 将水平辅助线的y坐标值赋值为相邻节点的上边界值。
+ *
+ * 3.2.计算是否靠近底部：
+ * 用当前节点的下边界值挨个减去其他节点的下边界值，如果小于24表示即将接近某个节点的底部，
+ * 将水平辅助线的y坐标值赋值为相邻节点的下边界值。
+ *
+ * 4.从下到上判断相邻节点
+ *
+ * 4.1.计算是否靠近底部：
+ * 用当前节点的上边界值挨个减去其他节点的下边界值，如果小于24表示即将接近某个节点的下方，
+ * 将水平辅助线的y坐标值赋值为相邻节点的下边界值。
+ *
+ * 4.2.计算是否靠近顶部：
+ * 用当前节点的上边界值挨个减去其他节点的上边界值，如果小于24表示即将接近某个节点的顶部，
+ * 将水平辅助线的y坐标值赋值为相邻节点的上边界值。
+ *
+ */
 const updateAlignment = () => {
   const { x: curX, y: curY, el, width: curW, height: curH } = store.dragData;
 
@@ -49,8 +100,14 @@ const updateAlignment = () => {
     return;
   }
 
+  let foundAlignment = false;
+
+  // 先计算当前元素的边界值
+  const curRight = curX + curW;
+  const curBottom = curY + curH;
+
   props.anodes.some(other => {
-    if (el.id == other.key) return false;
+    if (el.id === other.key) return false;
 
     const {
       x: targetX,
@@ -59,33 +116,48 @@ const updateAlignment = () => {
         style: { width: targetW, height: targetH },
       },
     } = other;
-    const targetRectRight = targetX + extractNumberFromString(targetW);
-    const targetRectBottom = targetY + extractNumberFromString(targetH);
 
-    // 水平对齐
-    if (
+    // 计算目标元素的边界值
+    const targetRight = targetX + extractNumberFromString(targetW);
+    const targetBottom = targetY + extractNumberFromString(targetH);
+
+    const topAlignMatch =
       checkIfWithinThreshold(curY, targetY) ||
-      checkIfWithinThreshold(curY + curH, targetRectBottom)
+      checkIfWithinThreshold(curBottom, targetY);
+    if (
+      topAlignMatch ||
+      checkIfWithinThreshold(curY, targetBottom) ||
+      checkIfWithinThreshold(curBottom, targetBottom)
     ) {
-      horizontalPosition.value = targetY;
+      horizontalPosition.value = topAlignMatch ? targetY : targetBottom;
       showHorizontalLine.value = true;
+      foundAlignment = true;
     } else {
       showHorizontalLine.value = false;
     }
 
     // 垂直对齐
-    if (
+    const leftAlignMatch =
       checkIfWithinThreshold(curX, targetX) ||
-      checkIfWithinThreshold(curX + curW, targetRectRight)
+      checkIfWithinThreshold(curRight, targetX);
+    if (
+      leftAlignMatch ||
+      checkIfWithinThreshold(curX, targetRight) ||
+      checkIfWithinThreshold(curRight, targetRight)
     ) {
-      verticalPosition.value = targetX;
+      verticalPosition.value = leftAlignMatch ? targetX : targetRight;
       showVerticalLine.value = true;
+      foundAlignment = true;
     } else {
       showVerticalLine.value = false;
     }
 
-    return showVerticalLine.value || showHorizontalLine.value;
+    return foundAlignment;
   });
+
+  if (!foundAlignment) {
+    resetAlignment();
+  }
 };
 
 watch(() => store.dragData, updateAlignment, { deep: true });
