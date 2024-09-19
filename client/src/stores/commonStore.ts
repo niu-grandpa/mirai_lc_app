2bfabdb06746304165b759ecf1b08aa10f998bcd
niu-grandpa <2864103063@qq.com>
2024-09-19 09:55:48 +0800

    feat：新增主题色状态

--- client/src/stores/commonStore.ts
+++ client/src/stores/commonStore.ts
@@ -1,3 +1,4 @@
+import commonConfig from '@/config/common';
 import { type UseDragAction, type UseDragTargetInfo } from '@/hooks/useDrag';
 import { defineStore } from 'pinia';
 
@@ -5,29 +6,32 @@ export interface DragState extends UseDragTargetInfo {
   action: UseDragAction;
 }
 
-interface CommonState {
-  _loading: boolean;
-  _dragData: DragState;
-}
+type Theme = 'dark' | 'light';
 
 export const useCommonStore = defineStore('common', {
-  state: (): CommonState => ({
-    _loading: false,
+  state: () => ({
+    _theme: commonConfig.theme as Theme,
+    _loading: commonConfig.loading,
     _dragData: { x: 0, y: 0, el: null, width: 0, height: 0, action: 'end' },
   }),
 
   getters: {
+    theme: state => state._theme,
     loading: state => state._loading,
     dragData: state => state._dragData,
   },
 
   actions: {
+    setTheme(value: Theme) {
+      this._theme = value;
+    },
+
     setLoading(value: boolean) {
       this._loading = value;
     },
 
     setDragData(data: DragState) {
-      this._dragData = data;
+      this._dragData = data as any;
     },
   },
 });
