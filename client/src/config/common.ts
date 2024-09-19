066278e5871b900686b7a4fd58bede3ccb889d2d
niu-grandpa <2864103063@qq.com>
2024-09-19 09:55:02 +0800

    feat：添加通用配置对象

---
+++ client/src/config/common.ts
@@ -0,0 +1,14 @@
+enum LOCAL_STORAGE_KEYS {
+  FILE_DATA = 'FILE_DATA',
+  OPENED_KEYS = 'OPENED_KEYS',
+  SELECTED_KEYS = 'SELECTED_KEYS',
+  EXPANED_KEYS = 'EXPANED_KEYS',
+  NOTIFICATION = 'NOTIFICATION',
+}
+
+export default {
+  theme: 'dark',
+  loading: false,
+  autoSaveInterval: 4000,
+  storageKeys: LOCAL_STORAGE_KEYS,
+};
