78ea1c36c9c492f454eb2f6baacf167e642b8aef
niu-grandpa <2864103063@qq.com>
2024-09-21 22:26:17 +0800

    feat：新增登录、注册、登出和注销接口

--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -4,7 +4,10 @@ import { Router } from 'express';
 
 const router = Router();
 
+router.get('/login', controller.login);
 router.post('/register', controller.register);
+router.post('/logout', authenticateUser, controller.logout);
+router.delete('/destroy', authenticateUser, controller.destory);
 router.post('/gen-anode-id', authenticateUser, controller.genANodeKey);
 
 export default router;
