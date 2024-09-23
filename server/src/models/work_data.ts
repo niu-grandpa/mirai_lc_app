f89bb25d2f6bf8b0b51552b4b8afe4255981aab2
niu-grandpa <2864103063@qq.com>
2024-09-23 10:28:14 +0800

    feat：新增ts类型继承

--- server/src/models/work_data.ts
+++ server/src/models/work_data.ts
@@ -1,7 +1,14 @@
+import { IReqHeaders } from '@/types/types';
+
 export interface WorkDataModel {
   id: number;
   userToken: string;
   rootKey: string;
   content: string;
   created_at: string;
+}
+
+export interface SyncWorkDataReq extends IReqHeaders {
+  rootKey: string;
+  content: string;
 }
