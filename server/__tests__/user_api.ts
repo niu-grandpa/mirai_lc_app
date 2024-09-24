15a433b57ca1a685c0e854c0285f77b9115a43cb
niu-grandpa <2864103063@qq.com>
2024-09-24 21:05:25 +0800

    test：测试逻辑变更

--- server/__tests__/user_api.ts
+++ server/__tests__/user_api.ts
@@ -1,13 +1,12 @@
 import { describe, expect, it } from '@jest/globals';
 import axios from 'axios';
-import logger from 'jet-logger';
 
 describe('user接口测试', () => {
   const phoneNumber = '13022222222';
-  const password = 'da1w6d54wafFWf1f613wa4wFw961231hrherWHJe8';
+  const password = '123456';
 
-  let userToken =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwidG9rZW4iOm51bGwsInBob25lTnVtYmVyIjoiMTMwMjIyMjIyMjIiLCJwYXNzd29yZCI6ImRhMXc2ZDU0d2FmRldmMWY2MTN3YTR3Rnc5NjEyMzFocmhlcldISmU4Iiwibmlja25hbWUiOiJ0ZXN0LXVzZXIiLCJpc1ZpcCI6MCwiYXZhdGFyIjpudWxsLCJpc0xvZ2luIjowLCJ2aXBFeHBpcmF0aW9uIjpudWxsLCJjcmVhdGVBdCI6IjIwMjQtMDktMjFUMTM6MDE6NTkuMDAwWiIsImlhdCI6MTcyNjkyMzcxOSwiZXhwIjoxNzI3MDEwMTE5fQ.xdJpjc8HnpGyWXgxSU4risYFm3XSkT72fezI0a5tv98';
+  let uid = 0;
+  let userToken = '';
 
   it('POST /api/user/register -> 应返回token', async () => {
     try {
@@ -20,13 +19,19 @@ describe('user接口测试', () => {
           nickname: 'test-user',
         }
       );
-      userToken = response.data.data;
+
       expect(response.status).toBe(200);
-      expect(typeof userToken).toBe('string');
+      expect(typeof response.data.data).toBe('object');
+
+      uid = response.data.data.id;
+      userToken = response.data.data.token;
     } catch (error) {
-      logger.err(
-        '[src/controllers/user.ts] 测试时请注释该行代码: password = this.encrypt(this.parse(password))'
-      );
+      if (error.status === 400) {
+        // 运行第二次测试时，需要手动从user表中选一个用户的uid和token
+        uid = 1;
+        userToken =
+          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZU51bWJlciI6IjEzMDIyMjIyMjIyIiwiaWF0IjoxNzI3MTcyMjM5LCJleHAiOjE3MjcyNTg2Mzl9.C6FhZzincDP-k61ikYtvTgfHUwtWMM5R_pq7x3Y9WcA';
+      }
       expect(error.status).toBe(400);
       expect(error.response.data.data).toEqual('该手机号已被注册');
     }
@@ -41,7 +46,6 @@ describe('user接口测试', () => {
         }
       );
       expect(response.status).toBe(200);
-      expect(typeof response.data.data).toBe('string');
     } catch (error) {
       expect(error.status).toBe(400);
       expect(error.response.data.data).toEqual('禁止重复登录');
@@ -58,16 +62,16 @@ describe('user接口测试', () => {
     expect(response.status).toBe(200);
   });
 
-  it('DELETE /api/user/destroy -> 注销用户', async () => {
-    const response = await axios.delete(
-      'http://localhost:8000/api/v1/user/destroy',
-      {
-        headers: { Authorization: userToken },
-        params: { phoneNumber },
-      }
-    );
-    expect(response.status).toBe(200);
-  });
+  // it('DELETE /api/user/destroy -> 注销用户', async () => {
+  //   const response = await axios.delete(
+  //     'http://localhost:8000/api/v1/user/destroy',
+  //     {
+  //       headers: { Authorization: userToken },
+  //       params: { uid, phoneNumber },
+  //     }
+  //   );
+  //   expect(response.status).toBe(200);
+  // });
 
   it('POST /api/user/gen-anode-id -> 生成节点key', async () => {
     const response = await axios.post(
--- server/__tests__/work_data_api.ts
+++ server/__tests__/work_data_api.ts
@@ -4,17 +4,20 @@ import logger from 'jet-logger';
 import mockData from '../static/mock_data.json';
 
 describe('work-data接口测试', () => {
-  const token =
-    'wYBvdjS02p6rHrUgzyMthlS57q1RyBLgRF8g1bWK4ZKrbmsHoHrGPpnRoQWKkusu';
+  // 先运行user_api测试后从user表任取一个uid和token
+  const uid = 1;
+  const userToken =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJwaG9uZU51bWJlciI6IjEzMDIyMjIyMjIyIiwiaWF0IjoxNzI3MTcyMjM5LCJleHAiOjE3MjcyNTg2Mzl9.C6FhZzincDP-k61ikYtvTgfHUwtWMM5R_pq7x3Y9WcA';
 
   it('PUT /api/work-data/sync 应返回状态码200', async () => {
     try {
       const response = await axios.put(
         'http://localhost:8000/api/v1/work-data/sync',
         {
-          headers: { Authorization: token },
-          rootKey: mockData.key,
-          content: JSON.stringify(mockData),
+          headers: { Authorization: userToken },
+          uid,
+          saveTime: Date.now(),
+          data: mockData,
         }
       );
 
@@ -28,12 +31,12 @@ describe('work-data接口测试', () => {
     const response = await axios.get(
       'http://localhost:8000/api/v1/work-data/all',
       {
-        headers: { Authorization: token },
+        headers: { Authorization: userToken },
+        params: { uid },
       }
     );
 
     expect(response.status).toBe(200);
-    expect(response.data).toHaveProperty('data[0].content');
-    expect(response.data.data[0].content).toEqual(mockData);
+    expect(response.data.data.data).toEqual(mockData);
   });
 });
