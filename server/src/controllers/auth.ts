ebf6a10627e35cb0cf143a7f80bd5c8d1b5f6af6
niu-grandpa <2864103063@qq.com>
2024-09-25 13:48:09 +0800

    feat：新增对用户token有效性进行验证，以及自动判断是否更新最新user token

--- server/src/controllers/auth.ts
+++ server/src/controllers/auth.ts
@@ -1,4 +1,6 @@
 import { RouteError, type IReq, type IRes } from '@/types/types';
+import { UserModel } from '@models/user';
+import { verifyJwtToken } from '@util/misc';
 import HttpStatusCodes from 'constants/http_status_codes';
 import { useDB } from 'database';
 import { NextFunction } from 'express';
@@ -19,21 +21,38 @@ export const authenticateUser = async <T>(
         .json({ data: '请先登录' });
     }
 
-    const [rows] = await useDB(
-      'SELECT COUNT(*) as count FROM users WHERE token=(?)',
-      [authorization]
+    const { exp, phoneNumber } = await verifyJwtToken<UserModel>(authorization);
+    const [user] = await useDB<UserModel>(
+      'SELECT * FROM users WHERE phoneNumber = (?)',
+      [phoneNumber]
     );
 
-    // @ts-ignore
-    //! 运行测试时需要注释以下判断逻辑
-    if (rows[0].count === 0) {
+    //* 运行非user接口测试时需要注释以下全部判断
+    if (!user) {
       return res
         .status(HttpStatusCodes.BAD_REQUEST)
-        .json({ data: '权限校验失败' });
+        .json({ data: '用户不存在' });
+    }
+
+    if (exp && exp < Date.now()) {
+      return res
+        .status(HttpStatusCodes.UNAUTHORIZED)
+        .json({ data: '登录已过期' });
+    }
+
+    if (user.token !== authorization) {
+      // 判断数据库的token是否需要更新
+      const { exp: oldExp } = await verifyJwtToken<UserModel>(authorization);
+      if (exp && oldExp && exp > oldExp) {
+        await useDB('UPDATE users SET token = (?) WHERE phoneNumber = (?)', [
+          authorization,
+          phoneNumber,
+        ]);
+      }
     }
 
     next();
   } catch (e) {
-    throw new RouteError(HttpStatusCodes.INTERNAL_SERVER_ERROR, e.message);
+    throw new RouteError(HttpStatusCodes.INTERNAL_SERVER_ERROR, '内部错误');
   }
 };
