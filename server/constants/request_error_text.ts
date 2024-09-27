7966c589f6ac3c1782c7e38964c740a082593138
niu-grandpa <2864103063@qq.com>
2024-09-27 12:31:42 +0800

    feat：添加http请求的request错误回复信息

---
+++ server/constants/request_error_text.ts
@@ -0,0 +1,15 @@
+enum RequestErrText {
+  OK = '操作成功',
+  NOT_LOGGED_IN = '用户未登录',
+  LOGIN_EXPIRED = '登录过期',
+  NOT_USERS = '用户不存在',
+  ERROR = '服务器错误',
+  MUST_JSON_FILE = '必须是json文件',
+  WRONG_PHONE_NUMBER = '手机号格式错误',
+  USER_EXISTS = '用户已存在',
+  REPEAT_LOGIN = '重复登录',
+  WRONG_PASSWORD = '密码不正确',
+  MISSING_PARAMS = '缺少必要参数',
+}
+
+export default RequestErrText;
