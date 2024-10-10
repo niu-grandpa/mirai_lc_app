f96891841eed8bbe5654b7ccead389d282584a70
niu-grandpa <2864103063@qq.com>
2024-10-10 21:56:45 +0800

    feat：新增404页面

---
+++ client/src/components/NotFound.vue
@@ -0,0 +1,9 @@
+<template>
+  <a-result status="404" title="404" sub-title="抱歉，您当前访问的页面不存在。">
+    <template #extra>
+      <router-link to="/">
+        <a-button type="primary">回到首页</a-button>
+      </router-link>
+    </template>
+  </a-result>
+</template>
