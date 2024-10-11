eaf3d576a26fde36bf59ba5714e60ddd90321b01
niu-grandpa <2864103063@qq.com>
2024-10-11 21:12:32 +0800

    feat：新增用户选项

--- client/src/views/WorkSpace/components/sidebar/RightOptions.vue
+++ client/src/views/WorkSpace/components/sidebar/RightOptions.vue
@@ -10,30 +10,77 @@
         <component :is="item.icon" />
       </div>
     </div>
+
     <div class="bottom">
-      <div
-        v-for="item in options.slice(-2)"
-        :class="['icon-box', { active: item.active }]"
-        :title="item.title"
-        :key="item.key"
-        @click="handleToogle(item.key)">
-        <component :is="item.icon" />
-      </div>
+      <template v-for="item in options.slice(-2)" :key="item.key">
+        <a-popover
+          v-if="item.key === SIDEBAR_OPTIONS.USER"
+          v-model:open="visible"
+          trigger="click"
+          :arrow="false"
+          placement="leftBottom">
+          <template #content>
+            <template v-if="!userStore.account">
+              <a-button type="text" @click="hide('/user/login')">
+                用户登录
+              </a-button>
+              <div />
+              <a-button type="text" @click="hide('/user/register')">
+                注册
+              </a-button>
+            </template>
+
+            <template v-else>
+              <a-button type="text" @click="hide('/user')">个人中心</a-button>
+              <div />
+              <a-button type="text">退出登陆</a-button>
+              <div />
+              <a-button type="text" danger>注销</a-button>
+            </template>
+          </template>
+
+          <div class="icon-box" :title="item.title">
+            <component :is="item.icon" />
+          </div>
+        </a-popover>
+
+        <div
+          v-else
+          :class="['icon-box', { active: item.active }]"
+          :title="item.title"
+          @click="handleToogle(item.key)">
+          <component :is="item.icon" />
+        </div>
+      </template>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import sidebarConfig, { SIDEBAR_OPTIONS } from '@/config/sidebar';
+import { useUserStore } from '@/stores/userStore';
 import { ref } from 'vue';
+import { useRouter } from 'vue-router';
 
 const emits = defineEmits<{
   (e: 'toogle', key: SIDEBAR_OPTIONS): void;
 }>();
 
+const router = useRouter();
+const userStore = useUserStore();
+
 const options = ref(sidebarConfig.options);
+const visible = ref<boolean>(false);
+
+const hide = (path?: string) => {
+  visible.value = false;
+  path && router.push(path);
+};
 
 const handleToogle = (key: SIDEBAR_OPTIONS) => {
+  if (key === SIDEBAR_OPTIONS.USER) {
+    return;
+  }
   options.value.forEach(item => {
     item.active = item.key === key;
   });
