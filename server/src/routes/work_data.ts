83c48b5fdf09b2730149eebefb708d17cf99f6e2
niu-grandpa <2864103063@qq.com>
2024-10-15 22:28:12 +0800

    feat：新增资源节点创建接口

--- server/src/routes/work_data.ts
+++ server/src/routes/work_data.ts
@@ -7,4 +7,8 @@ const router = Router();
 router.get('/all', authenticateUser, controller.getAll);
 router.put('/sync', authenticateUser, controller.syncData);
 
+router.post('/create-folder', controller.createFolderNode);
+router.post('/create-file', controller.createFileNode);
+router.post('/create-component', controller.createComponent);
+
 export default router;
