9eea4c3e039f50883f1da2d3e9058ed093beb1ea
niu-grandpa <2864103063@qq.com>
2024-10-21 19:54:54 +0800

    re: 自动保存间隔修改为1分钟

--- client/src/config/common.ts
+++ client/src/config/common.ts
@@ -1,6 +1,6 @@
 enum LOCAL_STORAGE_KEYS {
   USER_TOKEN = 'USER_TOKEN',
-  WORK_DATA = ' WORK_DATA',
+  WORK_DATA = 'WORK_DATA',
   OPENED_KEYS = 'OPENED_KEYS',
   SELECTED_KEYS = 'SELECTED_KEYS',
   EXPANED_KEYS = 'EXPANED_KEYS',
@@ -10,6 +10,6 @@ enum LOCAL_STORAGE_KEYS {
 export default {
   theme: 'dark',
   loading: false,
-  autoSaveInterval: 3000,
+  autoSaveInterval: 60000, // -> 60s
   storageKeys: LOCAL_STORAGE_KEYS,
 };
