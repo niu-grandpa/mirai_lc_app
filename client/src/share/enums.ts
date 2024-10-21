6244c578f52f48957a4e9eb5029a579afca4bc9b
niu-grandpa <2864103063@qq.com>
2024-10-21 19:57:06 +0800

    re：变量名称变更

--- client/src/share/enums.ts
+++ client/src/share/enums.ts
@@ -1,4 +1,4 @@
-export enum ANODE_ACTION_KEY {
+export enum NODE_ACTION_KEY {
   CREATE_PROJECT = 'project',
   CREATE_FOLDER = 'folder',
   CREATE_FILE = 'file',
@@ -12,16 +12,16 @@ export enum ANODE_ACTION_KEY {
   VIEW_CODE = 'viewCode',
 }
 
-export const ANodeActionTitles = {
-  [ANODE_ACTION_KEY.CREATE_FILE]: '新建页面...',
-  [ANODE_ACTION_KEY.CREATE_FOLDER]: '新建文件夹...',
-  [ANODE_ACTION_KEY.CUT]: '剪切',
-  [ANODE_ACTION_KEY.COPY]: '复制',
-  [ANODE_ACTION_KEY.PASTE]: '粘贴',
-  [ANODE_ACTION_KEY.IMPORT]: '导入',
-  [ANODE_ACTION_KEY.DOWNLOAD]: '下载',
-  [ANODE_ACTION_KEY.RENAME]: '重命名...',
-  [ANODE_ACTION_KEY.DELETE]: '删除',
+export const NodeActionTitles = {
+  [NODE_ACTION_KEY.CREATE_FILE]: '新建页面...',
+  [NODE_ACTION_KEY.CREATE_FOLDER]: '新建文件夹...',
+  [NODE_ACTION_KEY.CUT]: '剪切',
+  [NODE_ACTION_KEY.COPY]: '复制',
+  [NODE_ACTION_KEY.PASTE]: '粘贴',
+  [NODE_ACTION_KEY.IMPORT]: '导入',
+  [NODE_ACTION_KEY.DOWNLOAD]: '下载',
+  [NODE_ACTION_KEY.RENAME]: '重命名...',
+  [NODE_ACTION_KEY.DELETE]: '删除',
 };
 
 export enum DOWNLOAD_FILE_TYPE {
