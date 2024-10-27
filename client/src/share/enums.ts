24e80fdae4f88f69272449b062bbdb56568b2cf7
niu-grandpa <2864103063@qq.com>
2024-10-27 20:26:56 +0800

    feat：定义导出选项的枚举值

--- client/src/share/enums.ts
+++ client/src/share/enums.ts
@@ -12,13 +12,27 @@ export enum NODE_ACTION_KEY {
   VIEW_CODE = 'viewCode',
 }
 
+export enum EXPORT_OPTIONS {
+  VUE = 'vue',
+  REACT = 'react',
+  JSON = 'json',
+  HTML = 'html',
+}
+
+export const ExportOptions = {
+  [EXPORT_OPTIONS.VUE]: 'vue',
+  // [EXPORT_OPTIONS.REACT]: 'react',
+  [EXPORT_OPTIONS.JSON]: 'json',
+  [EXPORT_OPTIONS.HTML]: 'html',
+};
+
 export const NodeActionTitles = {
   [NODE_ACTION_KEY.CREATE_FILE]: '新建页面...',
   [NODE_ACTION_KEY.CREATE_FOLDER]: '新建文件夹...',
   [NODE_ACTION_KEY.CUT]: '剪切',
   [NODE_ACTION_KEY.COPY]: '复制',
   [NODE_ACTION_KEY.PASTE]: '粘贴',
-  [NODE_ACTION_KEY.IMPORT]: '导入',
+  [NODE_ACTION_KEY.IMPORT]: '导入 json',
   [NODE_ACTION_KEY.EXPORT]: '导出',
   [NODE_ACTION_KEY.RENAME]: '重命名...',
   [NODE_ACTION_KEY.DELETE]: '删除',
